769db295fc253a38c1c8074566f8fe0b925f074d
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-02-28 14:06:17 -0800

    update prototype file

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -23,4 +23,4 @@ typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s heap_t;
 
-
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
