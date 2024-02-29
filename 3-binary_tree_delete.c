cab28070a540a9a9f28541a92674fc1669a35bcc
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-02-29 15:07:47 -0800

    update 3-binary_tree

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,21 @@
+#include "binaree_trees.h"
+
+/**
+* binary_tree_delete - delete an entire tree
+* @tree: pointer to the root node of deleted tree
+* Return: nothing
+*/
+void binary_tree_delete(binary_tree_t *tree)
+{
+	binary_tree_t *root;
+
+	if (tree == NULL)
+		return;
+
+	root->n = tree;
+	root->parent = NULL;
+	root->left = NULL;
+	root->right = NULL;
+
+	return (tree);
+}
