a2464aa95b2e69e832c31bba3048a35595076627
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-02-29 15:24:54 -0800

    update 5-binary_tree

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,11 +1,11 @@
 #include "binary_trees.h"
 
 /**
-* binary_tree_is_leaf - checks if node is a leaf
+* binary_tree_is_root - checks if node is a root
 * @node: node to check
-* Return: 1 if node is a leaf, otherwise 0
+* Return: 1 if node is a root, otherwise 0
 */
-int binary_tree_is_leaf(const binary_tree_t *node)
+int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (node == NULL || node->left != NULL || node->right != NULL)
 		return (0);
