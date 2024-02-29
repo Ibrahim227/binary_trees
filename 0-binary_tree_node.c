8f49322ec23abd4dfeab9b6e0eb00bec2427bff4
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-02-29 12:22:47 -0800

    update 0-binary_tree_node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -8,11 +8,9 @@
 */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *root = *parent;
+	binary_tree_t *root;
 
 	root->parent;
 	value->root;
-	parent-> value;
-
-	return;
+	parent->value;
 }
