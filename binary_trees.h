b946bd2ef5d2600bb88b0520d88c6813a1af7e9e
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-02-29 12:54:48 -0800

    update prototype file

--- binary_trees.h
+++ binary_trees.h
@@ -50,8 +50,4 @@ static int print_t(const binary_tree_t *tree, int offset, int depth, char **s);
 static size_t _height(const binary_tree_t *tree);
 void binary_tree_print(const binary_tree_t *tree);
 
-
-
-
-
 #endif
