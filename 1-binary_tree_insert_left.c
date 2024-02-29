bc926728c5aff8e7327debf2218989a058492b0b
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-02-29 12:14:40 -0800

    update 1-binary_tree_insert

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_insert_left - insert a child to left
+* @parent: parent node
+* @value: integer
+* Return: apointer to created node
+*/
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+}
