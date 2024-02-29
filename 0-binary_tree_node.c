0df0c97e7797f8d041d71124ed09c5c4e570660a
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-02-29 12:51:26 -0800

    update 0-binary_tree_node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -8,7 +8,9 @@
 */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *root = malloc(sizeof(binary_tree_t));
+	binary_tree_t *root;
+
+	root = malloc(sizeof(binary_tree_t));
 
 	if (root == NULL)
 	{
