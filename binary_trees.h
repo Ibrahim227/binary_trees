5f2fd180659cea6da9e40f0b1ddfeb5ee4cba431
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-02-29 14:26:57 -0800

    udate prototypr file

--- binary_trees.h
+++ binary_trees.h
@@ -63,6 +63,6 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
 static int print_t(const binary_tree_t *tree, int offset, int depth, char **s);
 static size_t _height(const binary_tree_t *tree);
-
+void binary_tree_print(const binary_tree_t *tree);
 
 #endif
