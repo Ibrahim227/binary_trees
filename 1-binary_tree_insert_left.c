8d84a2f7e8833c1824214ace55178bf6d0e320a6
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-02-29 14:40:55 -0800

    update 1-binary_tree

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,7 +12,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	root = malloc(sizeof(binary_tree_t));
 
-	if (root = NULL)
+	if (root == NULL)
 	{
 		return (NULL);
 	}
