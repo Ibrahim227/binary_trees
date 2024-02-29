d5f3dc65557228bbf3dfeabe4fd105128f5b071e
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-02-29 12:14:16 -0800

    update 0-binary_tree_node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_node - insert a child to left
+* @parent: parent node
+* @value: integer
+* Return: apointer to created node
+*/
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+}
